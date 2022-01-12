0615915090210b07051d128947f435398ea5c415
Alba <alba.lp95@gmail.com>
2022-01-12 15:22:31 -0600

    Styles

--- total-look/src/components/Categories.js
+++ total-look/src/components/Categories.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import logo from '../img/logo.png'
 import login from '../img/login.png'
 import shoppingCar from '../img/shoppingCar.png'
+import edit from '../img/edit.png'
+import del from '../img/delete.png'
+import {db} from '../firebase/firebase-initialize'
+import { collection, getDocs } from "firebase/firestore";
+import { onSnapshot, query } from '@firebase/firestore'
 
 export default function Categories() {
+    const [clothes, setClothes] = useState([]);
+    useEffect(() => {
+        const renderClothes = () => {
+            //for (let i = 0; i < db.length; i++) {
+                try {
+                    onSnapshot(query(collection(db, '1')), (querySnapshot) => {
+                      const documents = [];
+                      querySnapshot.forEach((doc) => {
+                        documents.push({ id: doc.id, ...doc.data() });
+                      });
+                      setClothes(documents);
+                    });
+                  } catch (error) {
+                    console.log(error);
+                  }
+                  console.log('Jala', db.collection)
+           // }
+          
+          };
+          return renderClothes();
+        }, []);   
+
+          
     return (
         <div>
             <header>
@@ -21,26 +49,30 @@ export default function Categories() {
             </div>
             <div>
                 <div>
-                    <img src={shoppingCar} alt=""></img>
+                    <img src={'https://i.ibb.co/r3Vz74X/dress1.webp'} alt=""></img>
+                    <img src={edit} alt=""></img>
+                    <img src={del} alt=""></img>
+                </div>
+                <div>
+                    <img src={'https://i.ibb.co/pbzstpb/falda1-600x.webp'} alt=""></img>
+                    <img src={edit} alt=""></img>
+                    <img src={del} alt=""></img>
+                </div>
+                <div>
+                    <img src={'https://i.ibb.co/VWYRQtL/16173341445e7d36085b201d2fa94b9e21e42259c9.webp'} alt=""></img>
                     <img src={shoppingCar} alt=""></img>
                     <img src={shoppingCar} alt=""></img>
                 </div>
                 <div>
-                    <img src={shoppingCar} alt=""></img>
-                    <img src={shoppingCar} alt=""></img>
-                    <img src={shoppingCar} alt=""></img>
-                </div>
-                <div>
-                    <img src={shoppingCar} alt=""></img>
+                    <img src={'https://i.ibb.co/CKR630Q/1614061485b23832345e013c6ab88dd6c82429c47f.webp'} alt=""></img>
                     <img src={shoppingCar} alt=""></img>
                     <img src={shoppingCar} alt=""></img>
                 </div>
                 <div>
-                    <img src={shoppingCar} alt=""></img>
+                    <img src={'https://i.ibb.co/27JzvPF/1632830339c225b8952285cc29d26484576198939b-thumbnail-600x.webp'} alt=""></img>
                     <img src={shoppingCar} alt=""></img>
                     <img src={shoppingCar} alt=""></img>
                 </div>
-                
             </div>
             <footer>Realizado por Codeven</footer>
         </div>
--- total-look/src/components/Home.js
+++ total-look/src/components/Home.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import {Link } from "react-router-dom";
 import logo from '../img/logo.png'
-import desc from '../img/desc.png'
-import lupa from '../img/lupa.png'
+import '../components/scss/Home.scss'
 
 export default function Home() {
     return (
         <>
         <div>
-            <header>
+            <header className="head">
+                
               <img src={logo} alt="logo"></img>
-              <Link to ='/login'>Login</Link>
-              <Link to ='/categories'>Carrito</Link>
+              <div className="bar-icon">
+              <Link to ='/login'>
+              <img className="icons" src={"https://i.ibb.co/fFbLLM6/userblanco.png"} alt="login"></img></Link>
+              
+              <Link to ='/categories'>
+              <img className="icons" src= {"https://i.ibb.co/0QwqXZD/carrito.png"}alt="carrito"></img>
+              </Link>
+              </div>
             </header>
         </div>
         <div>
-            <h2>Promotions</h2>
-            <input type="search" id="site-search" name="q" aria-label="Search through site content"/>
-            <button><img src={lupa} alt="lupa"/></button>
-            <Link to ='/discounts'><img src={desc} alt="desc"></img></Link>
+            <h2 className="titulo">Promotions</h2>
+            <input className="search" type="search" id="site-search" name="q" aria-label="Search through site content" placeholder="Busca tu ropa"/>
+            <button><img className="icons" src={"https://i.ibb.co/bQQP6zx/lupa-2.png"} alt="lupa"/></button>
+            <div  className="promo"> <Link to ='/discounts'><img className="promoImg" src={"https://i.ibb.co/k68bDdC/desc.png"} alt="desc"></img></Link></div>
+           
         </div>
-        <div>
-            <h2>Categories</h2>
+        <div >
+            <h2 className="titulo">Categories</h2>
             <Link to ='/categories'>See all categories {'>>'} </Link>
         </div>
 
