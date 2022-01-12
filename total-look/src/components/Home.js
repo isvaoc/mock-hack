4e775ab9ed5a44b0d4832c8bb02fc2c475310485
Alicia Hernandez <hernndez.alicia4@gmail.com>
2022-01-12 00:34:21 -0600

    se agregan elementos al home

--- total-look/src/components/Home.js
+++ total-look/src/components/Home.js
@@ -8,17 +8,21 @@ export default function Home() {
     return (
         <>
         <div>
-             <header>
-                <img src={logo} alt="logo"></img>
-                <Link to ='/login'>Login</Link>
-                <Link to ='/categories'>Carrito</Link>
-             </header>
+            <header>
+              <img src={logo} alt="logo"></img>
+              <Link to ='/login'>Login</Link>
+              <Link to ='/categories'>Carrito</Link>
+            </header>
         </div>
         <div>
             <h2>Promotions</h2>
             <input type="search" id="site-search" name="q" aria-label="Search through site content"/>
             <button><img src={lupa} alt="lupa"/></button>
             <Link to ='/discounts'><img src={desc} alt="desc"></img></Link>
+        </div>
+        <div>
+            <h2>Categories</h2>
+            <Link to ='/categories'>See all categories >></Link>
         </div>
 
         </>
